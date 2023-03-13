7c2e2fa71466966e74226f3b0bf91f41f0d8a0c3
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-03-13 14:00:38 +1300

    Update 01_choose_rounds_v1.py

--- 01_choose_rounds_v1.py
+++ 01_choose_rounds_v1.py
@@ -4,17 +4,41 @@ from tkinter import *
 class ChooseRounds:
 
     def __init__(self):
+        self.intro_frame = Frame()
+        self.intro_frame.grid()
 
-        intro_frame = Frame()
-        intro_frame.grid()
+        self.start_title = Label(self.intro_frame, text="Colour Quest",
+                                 font=("Arial", "16", "bold"))
+        self.start_title.grid(row=0, padx=5, pady=5)
 
-        round_button = Button(width=12)
-        round_button.grid(intro_frame)
+        start_instructions_text = "In each round you will be given six different colours" \
+                                  " to choose from. Pick a colour and see if you can beat the" \
+                                  " computer's score!\n\nTo begin, choose how many rounds you'd" \
+                                  " like to play..."
+
+        self.start_instructions = Label(self.intro_frame, text=start_instructions_text,
+                                        wraplength=400, justify="left")
+        self.start_instructions.grid(row=1, padx=5, pady=5)
+
+        self.button_frame = Frame(self.intro_frame)
+        self.button_frame.grid(row=2)
+
+        self.round_button = Button(self.button_frame, text="3 Rounds",
+                                   width=12, bg="#f52a20")
+        self.round_button.grid(row=0, column=0, padx=5, pady=5)
+
+        self.round_button_2 = Button(self.button_frame, text="5 Rounds",
+                                     width=12, bg="#15e631")
+        self.round_button_2.grid(row=0, column=1, padx=5, pady=5)
+
+        self.round_button_3 = Button(self.button_frame, text="10 Rounds",
+                                     width=12, bg="#3027db")
+        self.round_button_3.grid(row=0, column=2, padx=5, pady=5)
 
 
 # main routine
 if __name__ == "__main__":
     root = Tk()
-    root.title("Temperature Converter")
+    root.title("Colour Game")
     ChooseRounds()
     root.mainloop()
