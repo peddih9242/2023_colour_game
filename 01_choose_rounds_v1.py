61f314e320b5b00d5a287546c1f7328782e113ed
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-03-14 11:05:01 +1300

    making play window

--- 01_choose_rounds_v1.py
+++ 01_choose_rounds_v1.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from functools import partial
 
 
 class ChooseRounds:
@@ -23,17 +24,57 @@ class ChooseRounds:
         self.button_frame = Frame(self.intro_frame)
         self.button_frame.grid(row=2)
 
-        self.round_button = Button(self.button_frame, text="3 Rounds",
-                                   width=12, bg="#f52a20")
-        self.round_button.grid(row=0, column=0, padx=5, pady=5)
+        button_fg = "#FFFFFF"
 
-        self.round_button_2 = Button(self.button_frame, text="5 Rounds",
-                                     width=12, bg="#15e631")
-        self.round_button_2.grid(row=0, column=1, padx=5, pady=5)
+        round_button_properties = [
+            ["#f52a20", 3],
+            ["#15e631", 5],
+            ["#3027db", 10]
+        ]
 
-        self.round_button_3 = Button(self.button_frame, text="10 Rounds",
-                                     width=12, bg="#3027db")
-        self.round_button_3.grid(row=0, column=2, padx=5, pady=5)
+        # create round buttons
+        for item in range(3):
+            self.round_button = Button(self.button_frame, text=f"{round_button_properties[item][1]} Rounds",
+                                       width=12, bg=round_button_properties[item][0], fg=button_fg,
+                                       command=lambda i=item: self.to_play(round_button_properties[i][1]))
+            self.round_button.grid(row=0, column=item, padx=5, pady=5)
+
+    def to_play(self, num_rounds):
+        Play(num_rounds)
+
+        # Hide root window (ie: hide rounds choice window)
+        root.withdraw()
+
+
+class Play:
+
+    def __init__(self, num_rounds):
+
+        self.play_box = Toplevel()
+
+        # if users press cross at top, closes help and
+        # 'releases' help button
+        self.play_box.protocol("WM_DELETE_WINDOW", partial(self.close_play))
+
+        self.play_frame = Frame(self.play_box, padx=10, pady=10)
+        self.play_frame.grid()
+
+        rounds_heading = "Choose - Round 1 of {}".format(num_rounds)
+        self.rounds_heading = Label(self.play_frame, text=rounds_heading,
+                                    font=("Arial", "16", "bold"))
+        self.rounds_heading.grid(row=0)
+
+        self.choice_frame = Frame(self.play_box, padx=10, pady=10)
+        self.choice_frame.grid(row=1)
+
+        for item in range(6):
+            pass
+
+    def close_play(self):
+        # reshow root (choose round) and destroy current box
+        # to allow new game to start
+        root.deiconify()
+        self.play_box.destroy()
 
 
 # main routine
